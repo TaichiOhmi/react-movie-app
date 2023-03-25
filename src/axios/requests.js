691a84b583d84046253d61f7fc01d6b546899934
TaichiOhmi <o6mi5.ta1@gmail.com>
2023-03-25 14:48:09 +0900

    youtubeでトレーラーを見れる機能を実装

--- src/axios/requests.js
+++ src/axios/requests.js
@@ -1,12 +1,12 @@
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 export const requests = {
-  feachTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
-  feachNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  feachTopRated: `/discover/tv?api_key=${API_KEY}&languager=en-us`,
-  feachActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28`,
-  feachComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35`,
-  feachHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27`,
-  feachRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749`,
-  feachDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99`,
+  feachTrending: `/trending/all/week?api_key=${API_KEY}&language=ja-J&region=ja-JP`,
+  feachNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213&language=ja-JP&region=ja-JP`,
+  feachTopRated: `/discover/tv?api_key=${API_KEY}&language=ja-JP&region=ja-JP`,
+  feachActionMovies: `/discover/tv?api_key=${API_KEY}&with_genres=28&language=ja-JP&region=ja-JP`,
+  feachComedyMovies: `/discover/tv?api_key=${API_KEY}&with_genres=35&language=ja-JP&region=ja-JP`,
+  feachHorrorMovies: `/discover/tv?api_key=${API_KEY}&with_genres=27&language=ja-JP&region=ja-JP`,
+  feachRomanceMovies: `/discover/tv?api_key=${API_KEY}&with_genres=10749&language=ja-JP&region=ja-JP`,
+  feachDocumentMovies: `/discover/tv?api_key=${API_KEY}&with_genres=99&language=ja-JP&region=ja-JP`,
 };
